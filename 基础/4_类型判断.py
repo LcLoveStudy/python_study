78f944a328410a5d4603bbfd77cb41fc91442d35
lichang <dotb116393@163.com>
2024-12-02 15:58:13 +0800

    :new:study isinstance func

--- 基础/4_类型判断.py
+++ 基础/4_类型判断.py
@@ -5,3 +5,9 @@ print(type(1.23))  # <class 'float'>
 print(type(True))  # <class 'bool'>
 print(type(None))  # <class 'NoneType'>
 print(type([1, 2, 3]))  # <class 'list'>
+
+# 判断一个变量是否是某个类型，可以使用isinstance()函数
+print(isinstance(123, int))  # True
+print(isinstance('123', str))  # True
+print(isinstance(1.23, float))  # True
+print(isinstance(True, bool))  # True
