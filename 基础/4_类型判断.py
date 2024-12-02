f9f5e214c2cbb92f9e0d11bd69fdd6901370db48
lichang <dotb116393@163.com>
2024-12-02 15:50:28 +0800

    :new:study type func

---
+++ 基础/4_类型判断.py
@@ -0,0 +1,7 @@
+# 想要判断一个变量或值的类型可以使用type()函数
+print(type(123))  # <class 'int'>
+print(type('123'))  # <class 'str'>
+print(type(1.23))  # <class 'float'>
+print(type(True))  # <class 'bool'>
+print(type(None))  # <class 'NoneType'>
+print(type([1, 2, 3]))  # <class 'list'>
