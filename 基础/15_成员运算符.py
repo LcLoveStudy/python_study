fb7c470076bc0ba585f28beb834b03491891ae12
lichang <dotb116393@163.com>
2024-12-02 17:19:22 +0800

    :new:study member

---
+++ 基础/15_成员运算符.py
@@ -0,0 +1,14 @@
+# in 
+print(3 in (1,2,3)) # True
+print('h' in 'hello') # True
+
+# not in
+print(3 not in (1,2,3)) # False
+print('h' not in 'hello') # False
+
+# is 
+print(3 is 3) # True
+print(3 is '3') # False
+
+# is not
+print(3 is not 3) # False
