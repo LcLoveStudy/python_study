33ff17c1e73002af7a41b3711e72dfd4f0279a8e
lichang <dotb116393@163.com>
2024-12-02 17:01:16 +0800

    :new:study compare

---
+++ 基础/13_比较运算符.py
@@ -0,0 +1,21 @@
+# 判断相等
+print(20 == '20')  # False
+print(20 == 20.0)  # True, python中整数和浮点数是相等的
+
+# 判断不相等
+print(20 != '20')  # True
+
+# 判断大于
+print(20 > 10)  # True
+
+# 判断小于
+print(20 < 10)  # False
+
+# 判断大于等于
+print(20 >= 10)  # True
+
+# 判断小于等于
+print(20 <= 10)  # False
+
+print('hello' < 'hi') # True, 字符串比较是依次对每个字符ASCII码值
+ord('c') #获取字符的ASCII码值
